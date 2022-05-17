43b926d6deb972d9619cb800c0161e4a2ba54e80
KLoOonal <kla2029@gmail.com>
2022-05-17 23:14:40 +0700

    add psudo responsive size for tablet, not perfect , phone size have no impact.

--- Assets/Scripts/DeviceData.cs
+++ Assets/Scripts/DeviceData.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeviceData : MonoBehaviour
+
+public enum ENUM_Device_Type
 {
-    // Start is called before the first frame update
-    void Start()
+    Tablet,
+    Phone
+}
+
+public static class DeviceData
+{
+    private static float DeviceDiagonalSizeInInches()
     {
-        
+        float screenWidth = Screen.width / Screen.dpi;
+        float screenHeight = Screen.height / Screen.dpi;
+        float diagonalInches = Mathf.Sqrt(Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2));
+
+        return diagonalInches;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static ENUM_Device_Type GetDeviceType()
     {
-        
+#if UNITY_IOS
+        bool deviceIsIpad = UnityEngine.iOS.Device.generation.ToString().Contains("iPad");
+                if (deviceIsIpad)
+                {
+                    return ENUMHERE.Tablet;
+                }
+     
+                bool deviceIsIphone = UnityEngine.iOS.Device.generation.ToString().Contains("iPhone");
+                if (deviceIsIphone)
+                {
+                    return ENUMHERE.Phone;
+                }
+#endif
+
+        float aspectRatio = Mathf.Max(Screen.width, Screen.height) / Mathf.Min(Screen.width, Screen.height);
+        bool isTablet = (DeviceDiagonalSizeInInches() > 6.5f && aspectRatio < 2f);
+
+        if (isTablet)
+        {
+            return ENUM_Device_Type.Tablet;
+        }
+        else
+        {
+            return ENUM_Device_Type.Phone;
+        }
     }
+
 }
--- Assets/Scripts/ScreenBaseSprite.cs
+++ Assets/Scripts/ScreenBaseSprite.cs
@@ -4,29 +4,113 @@ using UnityEngine;
 
 public class ScreenBaseSprite : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Setting")]
+    [SerializeField] private Vector2 defaultScreenSize = new Vector2(1080f, 2340f);
+    private SpriteRenderer sr;
+    private enum SpriteType
+    {
+        none,
+        FullScreen,
+        FillHorizontal,
+        FillVertical,
+        Ratio,
+        Ball,
+        Platform
+    }
+
+    [SerializeField] private SpriteType type;
+
+    void Awake()
+    {
+
+
+    }
+
     void Start()
     {
-        ResizeSpriteToScreen();
-    }
+        switch (type)
+        {
+            case SpriteType.FullScreen:
+                ResizeSpriteToFullScreen();
+                break;
 
-    // Update is called once per frame
-    void Update()
+            case SpriteType.FillHorizontal:
+                ResizeSpriteFillHorizontal();
+                break;
+
+            case SpriteType.FillVertical:
+                ResizeSpriteFillVertical();
+                break;
+
+            case SpriteType.Ratio:
+                ResizeSpriteToRatio();
+                break;
+
+            case SpriteType.none:
+                break;
+        }
+    }
+    private Vector2 GetScreenBound()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        return new Vector2(sr.sprite.bounds.size.x, sr.sprite.bounds.size.y);
     }
 
-    private void ResizeSpriteToScreen() {
-     SpriteRenderer sr = GetComponent<SpriteRenderer>();
-     if (sr == null) return;
-     
-     float width = sr.sprite.bounds.size.x;
-     float height = sr.sprite.bounds.size.y;
-     
-     float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
-     float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
-     
-     transform.localScale = new Vector2(worldScreenWidth / width , worldScreenHeight / height);
-     transform.position = new Vector3(0,0,2);
- }
+    private void ResizeSpriteToFullScreen()
+    {
+
+        float width = GetScreenBound().x;
+        float height = GetScreenBound().y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector2(worldScreenWidth / width, worldScreenHeight / height);
+        transform.position = new Vector3(0, 0, 2); // set to back when in edior.
+    }
+
+    private void ResizeSpriteToRatio()
+    {
+        if (DeviceData.GetDeviceType() == ENUM_Device_Type.Tablet)
+        {
+            Vector2 newScale = GetScaleByWidth();
+            if (type == SpriteType.Ball)
+            {
+                newScale = GetScaleByHeight();
+            }
+            if(type == SpriteType.Platform){
+                newScale = GetScaleByRatio();
+            }
+            Vector2 newScalePosition = GetScaleByRatio();
+            transform.localScale = Vector2.Scale(transform.localScale, newScale);
+            transform.position = Vector2.Scale(transform.position, newScalePosition);
+        }
+
+
+    }
+
+    private void ResizeSpriteFillHorizontal()
+    {
+
+    }
+
+    private void ResizeSpriteFillVertical()
+    {
+
+    }
+
+    private Vector3 GetScaleByWidth()
+    {
+        return new Vector3(Screen.width / defaultScreenSize.x, Screen.height / defaultScreenSize.x, 1);
+    }
+
+    private Vector3 GetScaleByHeight()
+    {
+        return new Vector3(Screen.width / defaultScreenSize.y, Screen.height / defaultScreenSize.y, 1);
+    }
+
+    private Vector3 GetScaleByRatio()
+    {
+        return new Vector3(Screen.width / defaultScreenSize.x, Screen.height / defaultScreenSize.y, 1);
+    }
 }
