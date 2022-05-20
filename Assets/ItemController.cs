74f0bf93adb11bcafc4b722491f56b5cfd5cc46e
KLoOonal <kla2029@gmail.com>
2022-05-20 20:37:36 +0700

    add power up icon , font and prepare to implement power up function.

--- Assets/ItemController.cs
+++ Assets/ItemController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
-    public enum ItemType{
+    public enum ItemType
+    {
         ball_speed_increase,
         ball_speed_decrease,
         ball_size_increase,
@@ -16,69 +17,123 @@ public class ItemController : MonoBehaviour
     }
 
     [Header("Setting")]
-    [SerializeField]private ItemType itemType;
+    [SerializeField] private ItemType itemType;
+    [SerializeField] private GameObject[] itemTypeImage;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private bool isActivated = false;
     private PlatformController platform;
 
-    void Awake(){
+    void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
 
-    void Start(){
+    void Start()
+    {
         rb.velocity = Vector2.down * 1.5f;
+        // set Image active base on type
     }
 
-    void OnTriggerEnter2D(Collider2D col){
-        if(isActivated){
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (isActivated)
+        {
             return;
-        }     
-        if(col.transform.tag == "Platform"){
+        }
+        if (col.transform.tag == "Platform")
+        {
             platform = col.transform.GetComponent<PlatformController>();
             ActivatePower();
             Despawn();
             isActivated = true;
         }
-        if(col.transform.tag == "DeadZone"){
+        if (col.transform.tag == "DeadZone")
+        {
             Despawn();
             isActivated = true;
-        } 
-    }
-
-    public void SetItemType(int randomType){
-        // set type by int value
-        // change image base on type
-    }
-
-    private void ActivatePower(){
-        // handle all case
-        switch(itemType){
-            case ItemType.ball_speed_increase:
-            break;
-            case ItemType.ball_speed_decrease:
-            break;
-            case ItemType.ball_size_increase:
-            break;
-            case ItemType.ball_size_decrease:
-            break;
-            case ItemType.platform_size_decrease:
-            break;
-            case ItemType.platform_size_increase:
-            break;
-            case ItemType.platform_size_full:
-            break;
-            case ItemType.platform_spawn_ball:
-            break;
         }
     }
 
-    private void Despawn(bool isDeadZone = false){
+    public void SetItemType(int randomType)
+    {
+        SetItemTypeFromIndex(randomType);
+        itemTypeImage[randomType].SetActive(true);
+    }
+
+    private void ActivatePower()
+    {
+        // handle all case
+        switch (itemType)
+        {
+            case ItemType.ball_speed_increase:
+                platform.BallSpeedModify(true);
+                break;
+            case ItemType.ball_speed_decrease:
+                platform.BallSpeedModify(false);
+                break;
+            case ItemType.ball_size_increase:
+                platform.BallSizeModify(true);
+                break;
+            case ItemType.ball_size_decrease:
+                platform.BallSizeModify(false);
+                break;
+            case ItemType.platform_size_decrease:
+                platform.PlatformSizeModify(false);
+                break;
+            case ItemType.platform_size_increase:
+                platform.PlatformSizeModify(true);
+                break;
+            case ItemType.platform_size_full:
+                platform.PlatformFull();
+                break;
+            case ItemType.platform_spawn_ball:
+                platform.PlatformSpawnBall();
+                break;
+        }
+    }
+
+    private void SetItemTypeFromIndex(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                itemType = ItemType.ball_speed_increase;
+                break;
+            case 1:
+                itemType = ItemType.ball_speed_decrease;
+                break;
+            case 2:
+                itemType = ItemType.ball_size_increase;
+                break;
+            case 3:
+                itemType = ItemType.ball_size_decrease;
+                break;
+            case 4:
+                itemType = ItemType.platform_size_decrease;
+                break;
+            case 5:
+                itemType = ItemType.platform_size_increase;
+                break;
+            case 6:
+                itemType = ItemType.platform_size_full;
+                break;
+            case 7:
+                itemType = ItemType.platform_spawn_ball;
+                break;
+            default:
+                itemType = ItemType.platform_size_increase;
+                break;
+        }
+    }
+
+    private void Despawn(bool isDeadZone = false)
+    {
         rb.velocity = Vector2.zero;
         rb.isKinematic = true;
         sr.color = Color.clear;
         ParticleController.Instance.ShowEffectItemCollect(this.transform.position);
-        Destroy(this.gameObject,5.0f);
+        Destroy(this.gameObject, 5.0f);
     }
 }
